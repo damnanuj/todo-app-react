48a703db32bd5063ba0023df56257b8d491a0bcf
Anuj <anujkrgupta21@gmail.com>
2024-04-08 23:18:39 +0530

    edit function

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,14 +4,14 @@ import React from 'react';
 
 
 
-const ToDo = ({ tasks, deleteTask, taskCompleted }) => {
+const ToDo = ({ tasks, deleteTask, taskCompleted, editTask }) => {
   return (
     <>
       {tasks.map(taskObj => (
         <div className="todo" key={taskObj.id}>
           <p className={`task ${taskObj.isDone ? 'completed' : ''}`} onClick={()=> taskCompleted(taskObj.id)}>{taskObj.task}</p>
           <div className="editDel">
-            <span className="material-icons">edit_square</span>
+            <span className="material-icons" onClick={()=>editTask(taskObj.id)}>edit_square</span>
             <span className="material-icons" onClick={() => deleteTask(taskObj.id)}>delete</span>
           </div>
         </div>
